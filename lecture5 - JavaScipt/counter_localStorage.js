2d1d3aceff0a610851677ab109919d55d9e35749
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2021-07-11 20:11:36 +0700

    Add JavaScript

---
+++ lecture5 - JavaScipt/counter_localStorage.js
@@ -0,0 +1,10 @@
+function count() {
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count;
+});
